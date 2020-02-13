468be114bcdfa297da4d7b795c88840f9a1b43cd
foreyes <foreyes_1002@163.com>
2020-02-13 14:02:06 +0800

    refract invader pair

--- default/role_invaderPair.js
+++ default/role_invaderPair.js
@@ -1,4 +1,37 @@
-var utils = require('utils');
+/*
+useage:
+    1. Ensure 'Run' function is executed every tick.
+    2. Call 'AttachInvaderPair(worker, healer, flagName)' to create an invader pair.
+        Eg. AttachInvaderPair(Game.getObjectById(workerId), Game.getObjectById(healerId), 'Destination');
+       They will move to the flag, and attack/dismantle it.
+    3. Change 'moveFunc' to your own move function.
+    4. Change 'moveInit' to your init function which will be called before move. If you don't need this part, change it into a blank function.
+
+PS:
+    1. If there is no such flag, worker will stop move and attack/dismantle.
+    2. Healer will follow worker, but it will still be attacked when closer to enemy. So you may need to remove the 
+       flag and adjust the position manually when worker is near to the target.
+    3. You can Re flag after arriving at the target place.
+*/
+
+/*
+Please edit these two functions!!!!!!!!!!!!!!!!!!
+Example:
+
+const moveFunc = (creep, target) => {
+    creep.moveTo(target);
+};
+const moveInit = () => {};
+*/
+
+const moveFunc = require('utils').DefaultMoveTo;
+const moveInit = function() {
+    require('cache');
+    worker.fetchCache();
+    healer.fetchCache();
+};
+
+//-------------------------------------------------------------------------------------------------------
 
 function getRunningPair(pairInMemory) {
     var worker = Game.getObjectById(pairInMemory.workerId);
@@ -6,10 +39,6 @@ function getRunningPair(pairInMemory) {
     var workFlag = Game.flags[pairInMemory.flagName];
     if(!worker || !healer) return null;
     return {worker: worker, healer: healer, workFlag: workFlag};
-}
-
-function isExit(pos) {
-    return pos.x == 0 || pos.y == 0 || pos.x == 49 || pos.y == 49;
 }
 
 function needMove(worker, workFlag) {
@@ -23,33 +52,32 @@ function moveWorker(worker, healer, workFlag) {
     // diff room
     if(worker.room.name != healer.room.name) {
         if(worker.room.name != workFlag.pos.roomName || needMove(worker, workFlag)) {
-            return utils.DefaultMoveTo(worker, workFlag);
+            return moveFunc(worker, workFlag);
         }
     } else {
         // wait for healer
         if(!worker.pos.isNearTo(healer.pos)) return;
         // console.log(worker, workFlag);
         if(worker.room.name != workFlag.pos.roomName || needMove(worker, workFlag)) {
-            return utils.DefaultMoveTo(worker, workFlag);
+            return moveFunc(worker, workFlag);
         }
     }
 }
 
 function moveHealer(worker, healer, workFlag) {
-    utils.DefaultMoveTo(healer, worker);
+    moveFunc(healer, worker);
 }
 
 function runPair(pair) {
-    console.log('Running invader pair: ' + Game.time);
+    // console.log('Running invader pair: ' + Game.time);
     var worker = pair.worker;
     var workerDamage = worker.hitsMax - worker.hits;
     var healer = pair.healer;
     var healerDamage = healer.hitsMax - healer.hits;
     var workFlag = pair.workFlag;
 
-    require('cache');
-    worker.fetchCache();
-    healer.fetchCache();
+    // init function for move
+    moveInit();
 
     // heal
     if(healer.room.name != worker.room.name) {
@@ -72,7 +100,7 @@ function runPair(pair) {
         }
     }
     // move
-    utils.DefaultMoveTo(healer, worker);
+    moveFunc(healer, worker);
     if(!workFlag) return;
     moveWorker(worker, healer, workFlag);
     // moveHealer(worker, healer, workFlag);
@@ -108,7 +136,7 @@ function Run() {
 }
 
 function AttachInvaderPair(worker, healer, flagName) {
-    worker.room.cache.boostPrepare = false;
+    // worker.room.cache.boostPrepare = false;
     if(Memory.invaderPairs == undefined) {
         Memory.invaderPairs = [];
     }
