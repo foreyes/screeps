1d9648db377e0141e4aa90220bdaa62d4303e492
foreyes <foreyes_1002@163.com>
2020-03-01 21:18:39 +0800

    clear const and config

--- default/const.js
+++ default/const.js
@@ -1,6 +1,2 @@
 module.exports = {
-	MyName: 'foreyes1001',
-    LabNum: 7,
-    MainRoomName: 'E33N36',
-    WallAndRampartHits: 10000000,
 };
--- default/powerCreep_config.js
+++ default/powerCreep_config.js
@@ -1,7 +1,2 @@
 module.exports = {
-	'The Jack': 'E29N34',
-	'The Queen': 'E33N36',
-	'The King': 'E35N38',
-	'The Joker': 'E26N31',
-	'The World': 'E29N33',
 };
